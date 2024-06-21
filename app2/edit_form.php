e6d1f095d09da62d0aa14b2d435d031d02ff6587
kai <cgk0910@gmail.com>
2024-06-21 10:41:54 +0800

    使用ajax來傳送表單資料進行新增

--- app2/edit_form.php
+++ app2/edit_form.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>新增學生</title>
+    <title>學生</title>
     <link rel="stylesheet" href="./css/bootstrap.css">
     <style>
         .form-label {
@@ -17,18 +17,22 @@
 <body>
     <div class="container">
         <h1 class="text-center">新增學生</h1>
-        <form class='form-group mx-auto col-6 mt-5'>
+        <form action="./api/insert.php" method="post" class='form-group mx-auto col-6 mt-5'>
+            <div class='d-flex my-2'>
+                <label for="" class='col-3 form-label'>id</label>
+                <input class="form-control" type="text" name="id" id="id">
+            </div>
             <div class='d-flex my-2'>
                 <label for="" class='col-3 form-label'>學號</label>
-                <input class="form-control" type="number" name="uni_id" id="uni_id">
+                <input class="form-control" type="text" name="uni_id" id="uni_id">
             </div>
             <div class='d-flex my-2'>
                 <label for="" class='col-3 form-label'>班級代號</label>
-                <input class="form-control" type="number" name="classroom" id="classroom">
+                <input class="form-control" type="text" name="classroom" id="classroom">
             </div>
             <div class='d-flex my-2'>
                 <label for="" class='col-3 form-label'>座號</label>
-                <input class="form-control" type="number" name="seat_num" id="seat_num">
+                <input class="form-control" type="text" name="seat_num" id="seat_num">
             </div>
             <div class='d-flex my-2'>
                 <label for="" class='col-3 form-label'>姓名</label>
@@ -36,7 +40,7 @@
             </div>
             <div class='d-flex my-2'>
                 <label for="" class='col-3 form-label'>生日</label>
-                <input class="form-control" type="date" name="birthday" id="birthday">
+                <input class="form-control" type="text" name="birthday" id="birthday">
             </div>
             <div class='d-flex my-2'>
                 <label for="" class='col-3 form-label'>身份證字號</label>
@@ -63,39 +67,14 @@
                 <input class="form-control" type="text" name="secondary" id="secondary">
             </div>
             <div>
-                <input type="button" value="送出" class='btn btn-primary' onclick="send()">
+                <input type="submit" value="送出" class='btn btn-primary'>
                 <input type="reset" value="重置" class='btn btn-warning'>
             </div>
         </form>
 
     </div>
-    <script src="./js/jquery-2.1.4.min.js"></script>
+    <script src="./js/jquery-2.1.4.js"></script>
     <script src='./js/bootstrap.js'></script>
 </body>
 
 </html>
-<script>
-    function send() {
-        let form = {
-            uni_id: $("#uni_id").val(),
-            classroom: $("#classroom").val(),
-            seat_num: $("#seat_num").val(),
-            name: $("#name").val(),
-            birthday: $("#birthday").val(),
-            national_id: $("#national_id").val(),
-            address: $("#address").val(),
-            parent: $("#parent").val(),
-            telphone: $("#telphone").val(),
-            major: $("#major").val(),
-            secondary: $("#secondary").val()
-        }
-        $.post("./api/insert.php", form, function(res) {
-            if (res == '1') {
-                alert('新增成功')
-                location.href = 'index.html'
-            } else {
-                alert('新增失敗')
-            }
-        })
-    }
-</script>
